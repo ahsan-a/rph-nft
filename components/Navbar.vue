f6ea52d9db95b6526a34b0011bd47615253a281f
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2022-01-30 21:38:27 +0000

    unfinished logout system

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import { useMainStore } from '~/store/main';
-import { getUser, useDiscordLogin } from '~/composables/discord';
+import { getUser, discordLogin } from '~/composables/discord';
+import { ref } from 'vue';
 
 const user = await getUser();
+const pfpEnabled = ref(false);
+const menuHover = ref(false);
 </script>
 
 <template>
-	<div class="fixed block w-full">
+	<div class="w-40 mt-16 py-1 px-0.5 bg-lightbg outline-midbg absolute rounded-md right-1" v-if="pfpEnabled">
+		<button class="text-white px-2 text-sm py-1 hover:bg-lighterbg transition-all" @click="">Logout</button>
+	</div>
+	<div class="w-full">
 		<div class="bg-blurple flex flex-row justify-between py-1">
 			<NuxtLink
 				class="flex flex-row items-center rounded-lg ml-2 hover:backdrop-brightness-50 hover:bg-white hover:bg-opacity-15 p-2 transition-colors pr-3"
@@ -18,11 +23,27 @@ const user = await getUser();
 			<div class="flex flex-row items-center mr-3">
 				<div v-if="user" class="flex flex-row items-center">
 					<h1 class="font-semibold text-white mr-3">{{ user.balance }} shitcoin</h1>
-					<img :src="user.avatar" class="w-11 h-11 rounded-full" />
+					<button
+						@click="pfpEnabled = !pfpEnabled"
+						@blur="pfpEnabled = false"
+						class="flex flex-row items-center px-3 transition-colors py-1 rounded-lg hover:bg-white hover:bg-opacity-15 bg-opacity-15"
+						:class="{ 'bg-white': pfpEnabled }"
+					>
+						<img :src="user.avatar" class="w-10 h-10 rounded-full object-cover" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							class="text-gray-100 fill-current w-3 h-auto ml-2"
+							viewBox="0 0 24 24"
+						>
+							<path d="M12 21l-12-18h24z" />
+						</svg>
+					</button>
 				</div>
 				<button
 					class="text-white font-bold text-lg hover:bg-white hover:bg-opacity-20 px-3 py-2 rounded-lg transition-colors"
-					@click="useDiscordLogin"
+					@click="discordLogin"
 					v-else
 				>
 					Sign In
@@ -30,5 +51,4 @@ const user = await getUser();
 			</div>
 		</div>
 	</div>
-	<div class="h-16"></div>
 </template>
--- composables/discord.ts
+++ composables/discord.ts
@@ -1,7 +1,7 @@
 import { User } from 'typings';
 import { APIUser } from 'discord-api-types/v9';
 
-export const useDiscordLogin = () => {
+export const discordLogin = () => {
 	const { DISCORD_CLIENT_ID } = useRuntimeConfig();
 	if (process.client) {
 		window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Foauth%2Fcallback&response_type=code&scope=identify%20guilds`;
@@ -13,3 +13,5 @@ export const getUser = async (): Promise<User | null> => {
 	if (!token.value) return null;
 	return (await $fetch('/api/user/getUser', { method: 'POST', params: { token: token.value } })) as User;
 };
+
+export const discordLogout = () => {};
