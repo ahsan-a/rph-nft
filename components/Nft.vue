dae888ec1e418e52d9c48344b431fbba8f6b29a5
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2022-02-12 19:48:46 +0000

    fixed image scaling issues

--- components/Nft.vue
+++ components/Nft.vue
@@ -15,7 +15,7 @@ const timeAgo = useTimeAgo(date);
 		class="bg-lightbg w-68 h-100 my-4 shadow-lg pt-7 px-6 pb-2 rounded-5xl border-2 border-midbg flex flex-col transition-all group-hover:bg-lighterbg"
 		:to="`/nft/${nft.id}`"
 	>
-		<img :src="`${SUPABASE_URL}/storage/v1/object/public/nfts/${nft.id}`" class="h-45 w-45 self-center rounded-4xl object-scale-down" />
+		<img :src="`${SUPABASE_URL}/storage/v1/object/public/nfts/${nft.id}`" class="h-45 w-45 self-center rounded-4xl object-cover" />
 		<h1 class="text-white font-bold text-2xl mt-2">{{ nft.name }}</h1>
 		<h1 class="text-white text-sm font-semibold">{{ nft.price }} shitcoin</h1>
 		<h2 class="text-light-400 mt-1 max-h-min overflow-hidden">{{ nft.description }}</h2>
